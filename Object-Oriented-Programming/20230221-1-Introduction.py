f8151eb6c774944fc4473a9345c41aae5c042379
Leo204_LKY <2498673113@qq.com>
2023-02-21 15:59:09 +0800

    Create 20230221-1-Introduction.py

---
+++ Object-Oriented-Programming/20230221-1-Introduction.py
@@ -0,0 +1,113 @@
+# JC1503 Object-Oriented Programming 面向对象程序设计
+# - Concepts
+# - Practice
+#
+# Developing Algorithms with OOP
+# - Data structures, algorithms
+# - Write more complex programs for problem solving
+# 
+# Software Engineering (write small/large programs)
+# - Design patterns
+# - Testing
+# - Use of packages and libraries
+
+# Why take this course?
+# A follow up course from JC1001 (Python Programming Foundation)
+# Preparation for the following/future programming courses
+# This is a programming course, so you learn to make things!!!
+
+# Course Team (AI)
+# Dr Tryphon Lambrou (Course Coordinator for AI)
+# - https://www.abdn.ac.uk/people/tryphon.lambrou
+# Dr Jinqu Zhang, Dr Lihua Cai (Teaching Buddy for AI)
+# See the details in MyAberdeen for how to contact us with questions and comments
+
+# Learning Outcomes
+# After this course you should:
+# - Have knowledge & understanding of the core concepts of, and common practices in, object-oriented programming.
+# - Have knowledge and understanding of fundamentals of object-oriented analysis and design, including a range of design patterns.
+# - Understand the need for principled approaches to programming problems, and the ability to think algorithmically: to take a problem and break it down into various components, translating these component problems into data structures and algorithms that can solve them.
+# - Be able to use existing programming tools, frameworks and systems to build, test and manage solutions.
+# - Be able to use UML notation to document software systems.
+
+# MyAberdeen has everything for JC1503
+# Use MyAberdeen as the main place to do your coursework
+# - Course information - books, contact details, etc
+# - Lecture materials
+# - Lecture recordings
+# - Practical exercises
+# - Practical solutions
+# - Assessments
+# - Links to your Codio workspaces - more on this later
+
+# Assessment
+# - Computer Programming Exercise (10%)
+# - Programming Assessment with Report (30%)
+# - Exam (60%)
+# Further details about the assignments including the assignment specifications and deadlines will be announced later in the course
+
+# Plagiarism 剽窃/抄袭
+# “Plagiarism” is defined by the University as the use, without adequate acknowledgment, of the intellectual work of another person in work submitted for assessment. A student cannot be found to have committed plagiarism where it can be shown that the student has taken all reasonable care to avoid representing the work of others as his or her own.
+# https://www.abdn.ac.uk/sls/online-resources/avoiding-plagiarism/
+# - Work should reflect your own intellectual effort.
+# - Others’ work should not be used, unless they are an authorized source.
+# - Assignment setters will determine what are, or are not, authorized sources.
+
+# Plagiarism in Source Code
+# “Source-code plagiarism in programming assignments can occur when a student reuses (Section IV-A) source-code authored by someone else and, intentionally or unintentionally, fails to acknowledge it adequately (Section IV-C), thus submitting it as his/her own work. This involves obtaining (Section IV-B) the source-code, either with or without the permission of the original author, and reusing (Section IV-A) source-code produced as part of another assessment (in which academic credit was gained) without adequate acknowledgement (Section IV-C). The latter practice, self-plagiarism, may constitute another academic offense”.
+# - http://web.science.mq.edu.au/~mtaylor/ponline/index.php?id=source-code-plagiarism
+#
+# An excellent guide: https://integrity.mit.edu/handbook/writing-code
+# - Never copy other student’s code.
+# - Don’t simply reuse code.
+# Cite the sources of code that you use within your code.
+# Ensure that any licensing conditions are adhered to.
+#
+# Don’t
+# - Change variable, constant or function/procedure names
+# - Simply replace language constructs
+# - Change indentation or separation within source code
+# Do
+# - Ensure that the source code isn’t a modification of somebody else’s work (equivalent to paraphrasing), it should be the result of your own intellectual effort. If you do find a useful snippet, then cite it in comments
+#
+# Your assessments will be run through a similarity checker that highlights plagiarism
+
+# Collusion & Contract Cheating 勾结 & 合同欺骗
+# - Collusion
+# "Collusion" is a form of plagiarism and is therefore treated in the same way as plagiarism. Ilt is definedas collaboration between students in an assignment that has not been authorised by the coursecoordinator. lt does not refer to authorised group work that is assessed by a single group report.
+# - Contract cheating
+# "Contract cheating" is an umbrella term to denote the submission of work by a student that has beenproduced by someone other than that student with the intention to deceive. This can be achievedthrough having another person, or commercial service, produce work that is subsequently submittedfor an assessment, whether that person/commercial service is paid or not.
+# https://www.abdn.ac.uk/staffnet/documents/academic-quality-handbook/Code%20of%20Practice%20in%20Student%20Discipline%20(Academic).pdf
+
+# Textbooks
+# Python 3 Object-Oriented Programming (Second Edition),
+#   - Dusty Phillips, PACKT Publishing, 2015.
+# Data Structures and Algorithms in Python,
+#   - Michael T. Goodrich et al, Wiley, 2013.
+# The Python Workbook: A Brief Introduction with Exercises and Solutions
+#   - Ben Stephenson, Springer, 2014
+# All books are available as e-books (courses) in the University Library with links to them in MyAberdeen.
+
+# Attendance & Monitoring
+# The Course Team will monitor:
+#   - attendance at practicals;
+#   - completion of in-course assignments.
+# If you fail to submit an assignment, or miss two (2) practicals you will be declared C6 'At Risk'.
+# Ultimately this can lead to C7 (refusal of class certificate) for the course. It is therefore in your own interest to do all you can to avoid this situation arising.
+# If you are ill or have any other good cause which prevents you from attending a tutorial/practical or completing in-course assessment, then it is vital that you inform the relevant authorities.
+
+# JC1503 will use Python 3
+# Modern Language
+#   - Easy to learn and use
+#   - Job (internship, career) opportunities
+# We will use Python 3 via Codio
+#   - Codio provides a cloud-based development environment for you, where you can do your coursework.
+
+# How to Learn Programming in General
+# There is no shortcut, spending time working on coding exercises, and writing programs is the only way.
+# - At least 1 hour every day in front of a computer, writing programs and making lots of mistakes (in addition to reading and thinking)
+# The 10,000 hours rule, and Deliberate Practice
+# - http://norvig.com/21-days.html
+# ...it takes about ten years to develop expertise in any of a wide variety of areas, including chess playing, ... painting, piano playing...
+# The key is deliberative practice: not just doing it again and again, but challenging yourself with a task that is just beyond your current ability, trying it, analyzing your performance while and after doing it, and correcting any mistakes. Then repeat. And repeat again. 
+# - Peter Norvig: http://norvig.com/21-days.html
