a806dfc52ef0d0130b9d124ccc3f12b488f06e73
Leo204_LKY <2498673113@qq.com>
2023-03-07 15:31:51 +0800

    Update 03-1-Inheritance-2.py

--- Object-Oriented-Programming/03-1-Inheritance-2.py
+++ Object-Oriented-Programming/03-1-Inheritance-2.py
@@ -62,3 +62,19 @@ class AmphibiousVehicle(Car, Boat):
 car1 = Car(ma='Audi',mo='Q7',yr=2004)
 boat1 = Boat(dis=200,blst=True)
 amph1 = AmphibiousVehicle(ma='VW',mo='Schwimmwagen',yr=1944,fp=True,dis=900,blst=True)
+
+# super() 允许多个继承层次结构中的相关类进行互操作
+# 但 Python 如何确定要在多个继承层次结构中的子类上使用哪个属性/方法版本？
+# - 任何指定的属性/方法在当前类中首先被搜索。
+# - 如果没有找到，则继续以深度优先、从左至右的方式搜索父类，而不会搜索同一类两次
+
+# Method Resolution Order (MRO，方法解释次序)
+# - 一组规则用于查找顺序到类搜索
+# - 顺序(Order)也称为类优先级列表或类的线性化
+# - 类的MRO可以被视为它的 __mro__ 属性或通过调用 mro() 方法: <classname>.mro()
+#   AmphibiousVehicle.mro()
+#   >>> [<class '__main__.AmphibiousVehicle'>, <class '__main__.Car'>, <class '__main__.Boat'>, <class 'object'>]
+
+
+# Using Instances as Attributes 使用实例作为属性
+# 继承(is-a)不是类之间关联的唯一方式，还有关联、组合(part-of)、聚合(has-a)
