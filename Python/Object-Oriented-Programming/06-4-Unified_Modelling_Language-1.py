fec9066eb06adb5c303d8f7ad4983ba7108b617b
Leo204_LKY <2498673113@qq.com>
2023-03-28 17:11:09 +0800

    Create 06-4-Unified_Modelling_Language-1.py

---
+++ Python/Object-Oriented-Programming/06-4-Unified_Modelling_Language-1.py
@@ -0,0 +1,18 @@
+# Lecture 6.4: Unified Modelling Language - Ⅰ | 统一设计语言Ⅰ
+
+# 使用 UML 的主要目的：
+# - 协助就正在开发的系统的分析和设计进行沟通
+# - support communication about the analysis and design of the system being developed
+# - 支持从“世界”中的问题域到机器中的解决方案域的移动
+# - support the movement from the problem domain in the "world" to the solution domain in the machine.
+
+
+# UML 的元素
+# “UML有关于如何绘制类模型、类之间的关联、对象之间发送的消息的规则。”
+# https://cloud.tencent.com/developer/article/1684161
+# 相关资料：UML Distilled, AddisonWesley, 2000
+
+# 全是图，py文件做不到😭
+# 略了，看课件
+
+# 使用 Lucidchart 来创建 UML：Lucidchart
