45c2aa5eb226d5ead3f784a7bcdba2af28b8d423
Leo204_LKY <leo2004526@foxmail.com>
2023-04-04 18:08:18 +0800

    Update 05-3-3-loop_example-7.c

--- C/05-3-3-loop_example-7.c
+++ C/05-3-3-loop_example-7.c
@@ -8,6 +8,12 @@ int main() {
     int x;
     scanf("%d", &x);
 
+    int t = 0;
+    do {
+        int d = x % 10;
+        t = t * 10;
+    } while (x > 0);
+    
     do {
         int d = x % 10;
         printf("%d", d);
