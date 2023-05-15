2a89fc21a6ef1647b3e1a1bf819855794098363b
Leo204-LKY <leo2004526@foxmail.com>
2023-05-15 22:34:07 +0800

    Create 07-2-1-linear_search.c

---
+++ C/07-2-1-linear_search.c
@@ -0,0 +1,30 @@
+// 搜索
+// 在一个数组中找到某个数的位置（或确认是否存在）
+
+// 线性搜索
+// 最基本的搜索方法是遍历
+
+#include <stdio.h>
+
+int search(int key, int a[], int len)
+{
+    int ret = -1;
+
+    for (int i = 0; i < len; i++) {
+        if (key = a[i]) {
+            ret = i;
+            break;
+        }
+    }
+
+    return ret;
+}
+
+int main()
+{
+    int a[] = {1, 3, 2, 5, 12, 14, 23, 6, 9, 45};
+    int r = search(12, a, sizeof(a)  / sizeof(a[0]));
+    printf("%d\n", r);
+
+    return 0;
+}
